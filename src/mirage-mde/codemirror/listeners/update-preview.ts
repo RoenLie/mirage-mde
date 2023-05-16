a9d62722b209783e796b098d1f5ab5d0a41c9c06
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-05-17 01:04:01 +0200

    Changed debounce to be a seperate function that does the update. update.docChanged changes so fast that a debounced value of it is unreliable.

--- src/mirage-mde/codemirror/listeners/update-preview.ts
+++ src/mirage-mde/codemirror/listeners/update-preview.ts
@@ -5,9 +5,10 @@ import { MirageMDE } from '../../mirage-mde.js';
 import { editorToPreview } from '../commands/toggle-sidebyside.js';
 
 
-export const updatePreviewListener = curryDebounce(500, (update: ViewUpdate, scope: MirageMDE) => {
-	if (scope.isSideBySideActive || scope.isWindowActive) {
-		if (update.docChanged)
-			editorToPreview(scope);
-	}
-});
+export const updatePreviewListener = (update: ViewUpdate, scope: MirageMDE) => {
+	if ((scope.isSideBySideActive || scope.isWindowActive) && update.docChanged)
+		bounced(scope);
+};
+
+
+const bounced = curryDebounce(500, (scope: MirageMDE) => editorToPreview(scope));
